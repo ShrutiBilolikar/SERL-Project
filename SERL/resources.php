6565f0b345081d4c340fcd179356d08fcd3c0f65
radhikag970 <115557846+radhikag970@users.noreply.github.com>
2023-03-21 10:40:15 +0530

    Add files via upload

---
+++ SERL/resources.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resources</title>
+</head>
+<body>
+    <div class="resources">
+        <h1>Resources</h1>
+        <div class="books">
+            <h4>Books</h4>
+             <ul>
+                <li><a href="https://www.amazon.in/Software-Engineering-Practitioners-Roger-Pressman/dp/0071267824">Software Engineering:A Practitioner's Approach.</a></li>
+                <li><a href="https://www.amazon.com/Software-Architecture-Practice-3rd-Engineering/dp/0321815734">Software Architecture in Practice (3rd Edition).</a></li>
+                <li><a href="https://www.amazon.in/Software-Metrics-Innovations-Engineering-Development/dp/1439838224?_encoding=UTF8&%2AVersion%2A=1&%2Aentries%2A=0&portal-device-attributes=desktop">Software Metrics:A Rigorous and Practical Approach,Third Edition in Practice.</a></li>
+                <li><a href="https://www.amazon.com/gp/product/0135974445/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0135974445&linkCode=as2&tag=aiop04-20&linkId=WYONYP5GDQFCFGZH">Agile Software Development, Principles,Patterns, and Practices.</a></li>
+                <li><a href="https://www.amazon.com/gp/product/0201633612/ref=as_li_tl?ie=UTF8&tag=jasonroell600-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=0201633612&linkId=a8396a795d3cc9a04acf269ef5c88eab">Design Patterns:Elements of Reusable Object-Oreinted Software.</a></li>
+             </ul>
+        </div>
+    </div>
+</body>
+</html>
