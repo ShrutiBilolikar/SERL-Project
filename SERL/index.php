fb37ae60b0597b8199525d549a09d157471b8b19
nayshasingh <singhnaysha03@gmail.com>
2023-04-03 10:10:58 +0530

    change index

--- SERL/index.php
+++ SERL/index.php
@@ -1,207 +1,116 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>SERL LAB</title>
+        <title>One Page Wonder - Start Bootstrap Template</title>
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Alkatra:wght@500&family=Dancing+Script&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
-        <link href="css/styles.css" rel="stylesheet" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="styles1.css" rel="stylesheet" />
     </head>
     <body id="page-top">
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-            <div class="container px-4 px-lg-5">
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+            <div class="container px-5">
+                <a class="navbar-brand" href="#page-top">SERL</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="#about">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#about">Faculty</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#about">Researcher</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#about">Publications</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#about">Projects</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#projects">Resources</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#signup">Contact us</a></li>
-                        <li class="nav-item"><a class="nav-link" href="main.php">LOGIN</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index1.php">HOMEPAGE</a></li>
+                        <li class="nav-item"><a class="nav-link" href="frontFaculty.php">FACULTY</a></li>
+                        <li class="nav-item"><a class="nav-link" href="frontResearcher.php">RESEARCHER</a></li>
+                        <li class="nav-item"><a class="nav-link" href="frontProject.php">PROJECTS</a></li>
+                        <li class="nav-item"><a class="nav-link" href="resources.php">PROJECTS</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact.php">CONTACT US</a></li>
+                        <li class="nav-item"><a class="nav-link" href="login">LOGIN</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
-
-        <header class="masthead">
-            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center " style="padding-top:5rem">
-                <div class="d-flex justify-content-center">
-                    <div class="text-center" style="background: rgba(255, 255, 255, 0.3)">
-                        <h1 style="font-size:3rem;font-family: 'Alkatra', cursive;">Software Engineering Research Laboratory</h1>
-                        <h2 class="text-black-50 mx-auto mt-2 mb-5"><b>Indian Institute of Information Technology Allahabad</b></h2>
-                        <a class="btn btn-primary" href="#about">Get Started</a> 
-                    </div>
+        <!-- Header-->
+        <header class="masthead text-center text-white">
+            <div class="masthead-content">
+                <div class="container px-5">
+                    <h1 class="masthead-heading mb-0">Software Engineering Research Laboratory</h1>
+                    <h2 class="masthead-subheading mb-0">Indian Institute of Information Technology</h2>
+                    <!-- <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Learn More</a> -->
                 </div>
             </div>
+           
         </header>
-                <br><br>
-                <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="https://cdni.iconscout.com/illustration/free/thumb/software-engineer-2043023-1731282.png" alt="..." /></div>
+        <!-- Content section 1-->
+        <section id="scroll">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="researcher1.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h2 class="display-4">About Us</h2>
+                            <p style="font-size :25px;">The Software Engineering Research Lab (SERL) at IIIT Allahabad is to be established to undertake 
+                                world-class research in designing of complex, largescale software-intensive systems of scalable 
+                                solutions, which are steered by business needs. The objective of the establishment of SERL@IIITA 
+                                is to cover cross-disciplinary research domains and to address real industrial software engineering 
+                                challenges.Recent developments in data driven industries, cyber physical systems, smart city applications 
+                                and business intelligence solutions have significant scope for software development industries and finding 
+                                solutions to underneath challenges.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Content section 2-->
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
                     <div class="col-lg-6">
-                        <div class="bg-black text-center h-100 project">
-                            <div class="d-flex h-100" style="background-color:#fff">
-                                <div class="project-text w-100 my-auto text-center text-lg-left" style="border-left:0.5rem solid #F9612C">
-                                    <h4 class="text-black">About Us</h4>
-                                    <p class="mb-0 text-black-50">The Software Engineering Research Lab (SERL) at IIIT Allahabad is to be established to undertake world-class research in designing of complex, largescale software-intensive systems of scalable solutions, which are steered by business needs. The objective of the establishment of SERL@IIITA is to cover cross-disciplinary research domains and to address real industrial software engineering challenges.Recent developments in data driven industries, cyber physical systems, smart city applications and business intelligence solutions have significant scope for software development industries and finding solutions to underneath challenges. </p>
-                                    <hr class="d-none d-lg-block mb-0 ms-0" />
-                                </div>
-                            </div>
-                        </div>
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="main.jpg" alt="..." /></div>
                     </div>
-                </div>
-                <br><br>
-                <div class="row gx-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="https://img.freepik.com/free-vector/editorial-commission-concept-illustration_114360-7751.jpg" alt="..." /></div>
-                    <div class="col-lg-6 order-lg-first" >
-                        <div class="bg-black text-center h-100 project">
-                            <div class="d-flex h-100" style="background-color:#fff">
-                                <div class="project-text w-100 my-auto text-center text-lg-right" style="border-right:0.5rem solid #F9612C">
-                                    <h4 class="text-black">Vision and Mission</h4><br>
-                                    <p class="mb-0 text-black-50">Big thinking precedes great achievement."</p><p>
-                        <br>
-The mission of this initiative is to establish a research lab of an industrial repute through various scholarly activities such as development of software tools, establishment of special interest groups, organizing conferences, workshops, forums for open source solutions and hands-on training in order to provide reliable and repeatable results to reflect our core strategy and vision of deepening excellence.Thus the establishment of Software Engineering Research Lab at IIITA may provide a strong platform to researchers and students to showcase their research potential world-wide.</p>
-                                    <hr class="d-none d-lg-block mb-0 me-0" />
-                                </div>
-                            </div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <h2 class="display-4">Vision and Mission</h2>
+                            <p style="font-size :25px;">Big thinking precedes great achievement. The mission of this initiative is to establish a research lab of an industrial 
+                                repute through various scholarly activities such as development of software tools, establishment of special interest groups, 
+                                organizing conferences, workshops, forums for open source solutions and hands-on training in order to provide reliable and 
+                                repeatable results to reflect our core strategy and vision of deepening excellence.Thus the establishment of Software 
+                                Engineering Research Lab at IIITA may provide a strong platform to researchers and students to showcase their research 
+                                potential world-wide.</p>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        <section class="contact-section bg-black" >
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5">
-                    <div class="col-md-4 mb-3 mb-md-0">
-                        <div class="card py-4 h-100">
-                            <div class="card-body text-center">
-                                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
-                                <h4 class="text-uppercase m-0">Address</h4>
-                                <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50">Indian Institute of Information Technology Allahabad
-
-Devghat, Jhalwa, Allahabad-211015, U. P. INDIA
-</div>
-                            </div>
+        <!-- Content section 3-->
+        <!-- <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="girl.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h2 class="display-4">Let there be rock!</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
                         </div>
                     </div>
-                    <div class="col-md-4 mb-3 mb-md-0">
-                        <div class="card py-4 h-100">
-                            <div class="card-body text-center">
-                                <i class="fas fa-envelope text-primary mb-2"></i>
-                                <h4 class="text-uppercase m-0">Email</h4>
-                                <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50"><a href="#!">contact@iiita.ac.in</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4 mb-3 mb-md-0">
-                        <div class="card py-4 h-100">
-                            <div class="card-body text-center">
-                                <i class="fas fa-mobile-alt text-primary mb-2"></i>
-                                <h4 class="text-uppercase m-0">Phone</h4>
-                                <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50">+91 (555) 902-8832</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="social d-flex justify-content-center" style="margin-top:1rem">
-                    <a class="mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                    <a class="mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                    <a class="mx-2" href="#!"><i class="fab fa-github"></i></a>
                 </div>
             </div>
-        </section>
-
-        <footer class="footer bg-black small text-center text-white-50" style="padding:1rem"><div class="container px-2 px-lg-2">© 2017 SERL - Software Engineering Research Lab</div></footer>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        </section> -->
+        <!-- Footer-->
+        <footer class="py-5 bg-black">
+            <div class="container px-5"><p class="m-0 text-center text-white small">Copyright &copy; SERL 2023</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
-        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
     </body>
-</html> -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    
-    <div class="header">
-           <div class="nav-menu">
-               <div class="logo">
-                <img src="serl.jpg" alt="null">
-               </div>
-               <div class="menu">
-                   <ul>
-                       <li><a href="#" class="active">Homepage</a></li>
-                       <li><a href="#">Faculty</a></li>
-                       <li><a href="#">Researcher</a></li>
-                       <li><a href="#">Projects</a></li>
-                       <li><a href="#">Contact us</a></li>
-                       <li><a href="main.php">Login</a></li>
-                   </ul>
-               </div>
-           </div>
-           <div class="banner"> 
-               <h1>Software Engineering Research Laboratory</h1>
-               <p>Indian Institute of Information Technology Allahabad</p>
-               <!-- <a href="index.html">Etiam posuere</a> -->
-           </div>
-    </div>
-    <div class="subheading">
-        <img src="researcher1.jpg" >
-        <div>
-            <h4 class="text-black">About Us</h4>
-                            <p class="mb-0 text-black-50">The Software Engineering Research Lab (SERL) at IIIT Allahabad is to be established to undertake world-class research in designing of complex, largescale software-intensive systems of scalable solutions, which are steered by business needs. The objective of the establishment of SERL@IIITA is to cover cross-disciplinary research domains and to address real industrial software engineering challenges.Recent developments in data driven industries, cyber physical systems, smart city applications and business intelligence solutions have significant scope for software development industries and finding solutions to underneath challenges. </p>
-                            <hr class="d-none d-lg-block mb-0 ms-0" />
-        </div>
-     </div>
-<div >
-    <img src="girl.jpg" alt="">
-                                    <h4 class="text-black">Vision and Mission</h4><br>
-                                    <p class="mb-0 text-black-50">Big thinking precedes great achievement."</p><p>
-                        <br>
-The mission of this initiative is to establish a research lab of an industrial repute through various scholarly activities such as development of software tools, establishment of special interest groups, organizing conferences, workshops, forums for open source solutions and hands-on training in order to provide reliable and repeatable results to reflect our core strategy and vision of deepening excellence.Thus the establishment of Software Engineering Research Lab at IIITA may provide a strong platform to researchers and students to showcase their research potential world-wide.</p>
-                                    <hr class="d-none d-lg-block mb-0 me-0" />
-                                </div>
-
-           <footer class="footer" >
-	<div class="waves">
-		<div class="wave" id="wave1"></div>
-		<div class="wave" id="wave2"></div>
-		<div class="wave" id="wave3"></div>
-		<div class="wave" id="wave4"></div>
-	</div>
-
-	<ul class="menu">
-		<li class="menu__item"><a class="menu__link" href="#">Home</a></li>
-		<li class="menu__item"><a class="menu__link" href="#">About us</a></li>
-		<li class="menu__item"><a class="menu__link" href="#">Contact us</a></li>
-
-
-	</ul>
-	<p style="opacity: 0.75;">Made with 🤍 by Khawaja</p>
-
-</footer>
-    </div>
-</body>
 </html>
